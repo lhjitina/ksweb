acd78385a6fa80832154ab5f97621daf1d6c15aa
lhj <hejunliu@hotmail.com>
2018-12-25 07:22:45 +0800

    change passwd

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { NzModalRef, NzModalService, NzMessageService } from 'ng-zorro-antd';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-portal',
@@ -8,12 +11,47 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class PortalComponent implements OnInit {
 
-  userName: string = "guest";
+  user: string = "guest";
+  bShowPasswdModal: boolean = false;
+  passwdFormGroup: FormGroup;
+  processing: boolean = false;
 
-  constructor(private cookie: CookieService) { }
+  constructor(private cookie: CookieService,
+              private modal: NzModalService,
+              private fb: FormBuilder,
+              private http: HttpClient,
+              private message: NzMessageService) { 
+    this.passwdFormGroup = this.fb.group({
+      old: [''],
+      new: ['']
+    });
+              }
 
   ngOnInit() {
-    this.userName = this.cookie.get("userName");
+    console.log("portal init")
+    this.user = this.cookie.get("loginName");
+    console.log("user:"+this.user)
+  }
+  onModifyPasswd(): void{
+    console.log("modify passwd")
+    this.bShowPasswdModal = true;
   }
 
+  nzOnCancel(): void{
+    this.bShowPasswdModal = false;
+    this.passwdFormGroup.patchValue({odl: ''});
+    this.passwdFormGroup.patchValue({new: ''});
+  }
+
+  onOk(): void{
+    this.http.post("/api/passwd/modify", this.passwdFormGroup.value).subscribe((res: any)=>{
+      if (res == "ok"){
+        this.message.create('success', '密码修改成功.')
+        this.bShowPasswdModal = false;
+      }
+      else{
+        this.message.create('error', '密码修改失败!');
+      }
+    })
+  }
 }
